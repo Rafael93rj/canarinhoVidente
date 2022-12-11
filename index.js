ad4cb0b1fa9194297dba235e02b83d2f1813c135
Rafael Nascimento <rafael.93rj@gmail.com>
2022-12-11 12:40:02 -0300

    modifica logica que valida a pergunta

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const elementoResposta = document.querySelector("#resposta")
 const inputPergunta = document.querySelector("#inputPergunta")
 const buttonPerguntar = document.querySelector('#buttonPerguntar')
-const respostas = [ "Certeza!",
+const respostas = ["Certeza!",
   "Não tenho tanta certeza.",
   "É decididamente assim.",
   "Não conte com isso.",
@@ -22,20 +22,23 @@ const respostas = [ "Certeza!",
   "Sinais apontam que sim.",]
 
 function fazerPergunta() {
-  if(inputPergunta.value == "") {alert("Digite sua pergunta")
-  return
+  if (inputPergunta.value == "" || inputPergunta.value == " ") {
+    alert("Pergunta inválida!")
+    return
   }
-buttonPerguntar.setAttribute("disalble", true)
+
+  buttonPerguntar.setAttribute("disalble", true)
   const pergunta = "<div>" + inputPergunta.value + "</div>"
 
   const totalRespostas = respostas.length
-  const numeroAleatorio = Math.floor(Math.random () * totalRespostas)
-  
+  const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
+
   elementoResposta.innerHTML = pergunta + respostas[numeroAleatorio]
   elementoResposta.style.opacity = 1;
-  setTimeout(function() {elementoResposta.style.opacity = 0;
-  buttonPerguntar.removeAttribute("disable")
+  setTimeout(function () {
+    elementoResposta.style.opacity = 0;
+    buttonPerguntar.removeAttribute("disable")
   }, 3000)
-  }
+}
 
 
